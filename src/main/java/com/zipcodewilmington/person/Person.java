8933540efdb5954081efde4da5a5f14a592190a3
Ethan <ethan@zipcoders-MacBook-Pro-4.local>
2022-03-04 13:49:36 -0500

    done with testing

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,30 +6,79 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private boolean hasLicense;
+    private double height;
+    private int weight;
+    private boolean hasChildren;
+    private String address;
 
     public Person() {
+        this.name = "";
+        this.age = Integer.MAX_VALUE;
+        this.address = "";
+        this.weight = 0;
+        this.height = 4.0;
+        this.hasLicense = false;
+        this.hasChildren = false;
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name =name;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public Person(String name, int age, String address,
+                  int weight, double height, boolean hasLicense,boolean hasChildren) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.weight = weight;
+        this.height = height;
+        this.hasChildren = hasChildren;
+        this.hasLicense = hasLicense;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
 
-    public String getName() {
-        return null;
-    }
+    public void setAddress(String address) {this.address = address;}
+
+    public void setWeight(int weight ){ this.weight = weight;}
+
+    public void setHeight(double height){this.height = height;}
+
+    public void setLicense(boolean license){this.hasLicense = license;}
+
+    public void setChildren(boolean hasChildren){this.hasChildren = hasChildren;}
+
+    public String getName() { return this.name;}
 
     public Integer getAge() {
-        return null;
+        return this.age;
     }
+
+    public String getAddress() {return this.address;}
+
+    public int getWeight( ){ return this.weight;}
+
+    public double getHeight(){return this.height;}
+
+    public boolean isLicenseTrue(){return this.hasLicense;}
+
+    public boolean isChildrenTrue(){return this.hasChildren;}
 }
+
+
+
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -69,6 +69,29 @@ public class TestPerson {
     }
 
     @Test
+    public void testConstructorWithAllFeilds() {
+        // Given
+        Integer expectedAge = 5;
+        String expectedName = "Leon";
+        String expectedAddress = "123 Main St.";
+        Integer expectedWeight = 115;
+        Double expectedHeight = 6.2;
+        boolean hasLicense = true;
+        boolean hasChildren = false;
+
+        // When
+        Person person = new Person(expectedName, expectedAge,expectedAddress,expectedWeight,
+                expectedHeight,hasLicense,hasChildren);
+
+        // Then
+        Integer actualAge = person.getAge();
+        String actualName = person.getName();
+
+        Assert.assertEquals(expectedAge, actualAge);
+        Assert.assertEquals(expectedName, actualName);
+    }
+
+    @Test
     public void testSetName() {
         // Given
         Person person = new Person();
@@ -95,4 +118,75 @@ public class TestPerson {
         Integer actual = person.getAge();
         Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void testSetAddress() {
+        // Given
+        Person person = new Person();
+        String expected = "123 Main St";
+
+        // When
+        person.setAddress(expected);
+
+        // Then
+        String actual = person.getAddress();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetWeight() {
+        // Given
+        Person person = new Person();
+        Integer expected = 100;
+
+        // When
+        person.setWeight(expected);
+
+        // Then
+        Integer actual = person.getWeight();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetHeight() {
+        // Given
+        Person person = new Person();
+        Double expected = 5.5;
+
+        // When
+        person.setHeight(expected);
+
+        // Then
+        Double actual = person.getHeight();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetLicense() {
+        // Given
+        Person person = new Person();
+        boolean expected = true;
+
+        // When
+        person.setLicense(expected);
+
+        // Then
+        boolean actual = person.isLicenseTrue();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetChildren() {
+        // Given
+        Person person = new Person();
+        boolean expected = false;
+
+        // When
+        person.setChildren(expected);
+
+        // Then
+        boolean actual = person.isChildrenTrue();
+        Assert.assertEquals(expected, actual);
+    }
+
 }
